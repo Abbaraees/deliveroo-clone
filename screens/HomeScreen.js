86ba8f41dff2626b7995b0af76a98318dfbb252a
Muhammad Lawal <abbaraees@gmail.com>
2024-01-04 22:18:49 +0100

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text className="text-red-400">Home Screen</Text>
+        </View>
+    )
+}
